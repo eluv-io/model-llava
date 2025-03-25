31e1d35e304c46dec8c94abdc6a8a078996df164
elv-joeA <joe.anakata@eluv.io>
2025-03-25 17:20:25 +0000

    this is not a test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo 'no test for llava right now since it needs ollama'
+
+echo 'maybe set up fake ollama in the future'
+
+echo 'or assume we have one (but check it first before testing)'
+
+
+
