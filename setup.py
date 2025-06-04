5953d01b3a46119f8250defbac5d2e139778d993
elv-joeA <joe.anakata@eluv.io>
2025-06-04 15:20:19 -0700

    update repo locations

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'ollama',
         'pillow',
         'elv-client-py @ git+https://github.com/eluv-io/elv-client-py.git#egg=elv-client-py',
-        'quick_test_py @ git+https://github.com/eluv-io/quick_test_py.git#egg=quick_test_py',
+        'quick_test_py @ git+https://github.com/eluv-io/quick-test-py.git#egg=quick_test_py',
         'common_ml @ git+ssh://git@github.com/eluv-io/common-ml.git#egg=common_ml',
     ],
 )
